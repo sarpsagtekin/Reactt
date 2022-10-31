d2ec3809a70b1bd2989e17428be574d885d55c96
sarp <sarpsagtekin@gmail.com>
2022-10-31 11:01:35 +0000

    test

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,15 @@
-function MovieDetails() {
+
+import { useParams } from "react-router-dom";
+
+function MovieDetails({movies}) {
+  const { movieId } = useParams();
+  const choice = movies.find((movie) => {
+    return movie.id = movieId;
+  })
   return (
     <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+      <h1>{choice.title}</h1>
+      <p>{choice.description}</p>
     </div>
   );
 }
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,27 @@
-function Movies() {
+import MovieDetails from "../MovieDetails"
+import { Route, Switch, NavLink } from "react-router-dom";
+
+function Movies({ movies }) {
   return (
     <div className='comp orange'>
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movie) => {
+          let url="/movies/".concat(movie.id);
+          return (
+            <NavLink key={movie.id} to={url}>
+              {movie.title}
+            </NavLink>
+          )
+        })}
+      </nav>
+
+
+      <Switch>
+        <Route path="/movies/:movieID">
+          <MovieDetails movies={movies}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
